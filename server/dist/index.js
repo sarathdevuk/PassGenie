13eadebd1015de0980bb7292537834e8d9a96273
Sarathdev <sarathdev0@gmail.com>
2023-08-04 23:17:30 +0530

    user login route done

--- server/dist/index.js
+++ server/dist/index.js
@@ -9,6 +9,7 @@ const cors_1 = __importDefault(require("cors"));
 const path_1 = __importDefault(require("path"));
 const morgan_1 = __importDefault(require("morgan"));
 const userRoute_1 = __importDefault(require("./routes/userRoute"));
+const database_1 = __importDefault(require("./config/database"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 const port = process.env.PORT || 5050;
@@ -22,7 +23,10 @@ app.use((0, cors_1.default)({
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     credentials: true,
 }));
+// User Router middleware setup 
 app.use("/", userRoute_1.default);
+// connecting to database 
+(0, database_1.default)();
 app.listen(port, () => {
     return console.log(`Express is listening at http://localhost:${port}`);
 });
--- server/src/routes/userRoute.ts
+++ server/src/routes/userRoute.ts
@@ -1,11 +1,15 @@
 import _default from "concurrently";
 import { Router } from "express" ;
+import { doLogin, register } from "../controller/userController";
 
 const router = Router() ;
 
 router.get("/" , (req, res) => {
   res.send('Hello useroruter is working')
 } )
+
+router.post("/register",register)
+router.post("/login",doLogin)
 
 
 export default router
