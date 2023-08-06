a48e9d17ff6cb4560947bc0bdc23f0045bc63629
Sarathdev <sarathdev0@gmail.com>
2023-08-06 21:58:27 +0530

    axios instance created for data transfer

---
+++ client/src/axios/axios.ts
@@ -0,0 +1,31 @@
+import axios , {AxiosError , AxiosResponse}  from 'axios' ;
+
+
+const axiosInstance = () => {
+  const instance = axios.create({
+    baseURL:"http://localhost:5050",
+    timeout : 5000 ,
+    headers: {
+      "Content-Type" : "application/json" ,
+    } ,
+  }) ;
+
+  // instance request interceptor
+  instance.interceptors.request.use((request) => {
+    const token = localStorage.getItem("jwtToken") ;
+    if(token) {
+      request.headers.Authorization = `Bearer ${token}`
+     }
+     return request
+  })
+
+  
+  //instance response interceptor
+  instance.interceptors.response.use(
+    (response: AxiosResponse) => response ,
+    (error: AxiosError) =>  Promise.reject(error.response?.data )
+  ) 
+return instance 
+}
+
+export default axiosInstance; 
