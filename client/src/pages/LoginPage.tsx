4b5a98e6cddf08e5a963f8520a1c023d7860aac8
Sarathdev <sarathdev0@gmail.com>
2023-08-06 21:59:44 +0530

    Signup page created

---
+++ client/src/pages/LoginPage.tsx
@@ -0,0 +1,40 @@
+import { useNavigate } from 'react-router-dom'
+import AuthForm from '../componants/AuthForm'
+import { login } from '../services/userApi'
+import { toast } from 'react-hot-toast';
+
+function LoginPage() {
+  const navigate = useNavigate() ;
+
+  const handleLogin = (values : object) => { 
+    console.log(values)
+    login(values).then((response)=> {
+      console.log(response.data , "success");
+      
+      localStorage.setItem('jwtToken' , response.data?.token)
+      navigate('/home')
+    })
+    .catch((err) => {
+      console.log(err);
+      
+      toast.error(err.error , {
+        position: "top-center",
+      })
+    })
+  }
+
+  return (
+    <AuthForm 
+    header={"Login"}
+    redirect={"/signup"}
+    buttonName={"Login"}
+    redirectBtnName={"Sign up"}
+    handleFunction={handleLogin}
+    formType={'login'}
+    >
+    </AuthForm>
+
+  )
+}
+
+export default LoginPage
