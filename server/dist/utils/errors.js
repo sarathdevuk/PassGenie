638902143fd0b0137f68d824bd61c23d4968909d
Sarathdev <sarathdev0@gmail.com>
2023-08-08 20:20:47 +0530

    add comments for error

--- server/dist/utils/errors.js
+++ server/dist/utils/errors.js
@@ -1,8 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+// Define a custom error class 'AppError' that extends the built-in 'Error' class
 class AppError extends Error {
+    // Constructor function for the 'AppError' class
     constructor(statusCode, message) {
-        super(message);
+        super(message); // Call the parent constructor (Error) with the provided message
+        // Set the prototype of the current instance to be an instance of 'AppError'
         Object.setPrototypeOf(this, new.target.prototype);
         this.statusCode = statusCode;
     }
--- server/src/utils/errors.ts
+++ server/src/utils/errors.ts
@@ -1,8 +1,11 @@
+// Define a custom error class 'AppError' that extends the built-in 'Error' class
 class AppError extends Error {
   statusCode : number;
+  // Constructor function for the 'AppError' class
   constructor(statusCode : number , message : string ) {
-    super(message);
+    super(message); // Call the parent constructor (Error) with the provided message
 
+     // Set the prototype of the current instance to be an instance of 'AppError'
     Object.setPrototypeOf(this, new.target.prototype) ;
     this.statusCode = statusCode ;
   }
