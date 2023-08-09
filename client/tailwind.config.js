83817c0a2686528e0187908cb2571208d8909ac5
Sarathdev <sarathdev0@gmail.com>
2023-08-09 16:41:51 +0530

    tailwind configured for using daisy ui

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", 
-  "./src/**/*.{js,ts,jsx,tsx}"],
+  "./src/**/*.{js,ts,jsx,tsx}" ,
+  "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("daisyui") , 
+  require('flowbite/plugin')  ],
 };
