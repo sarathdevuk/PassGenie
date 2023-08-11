f0ce8ba60bb2f4a9439deb0af2ed5d71b9e403a1
Sarathdev <sarathdev0@gmail.com>
2023-08-11 10:38:14 +0530

    Setup Home page

---
+++ client/src/pages/HomePage.tsx
@@ -0,0 +1,18 @@
+import Banner from "../componants/Banner"
+import Header from "../componants/Header"
+
+function HomePage() {
+  return (
+
+    
+    <div>
+        <div >
+          <Header />
+           <Banner/>
+        </div>
+        
+      </div>
+  ) 
+}
+
+export default HomePage
